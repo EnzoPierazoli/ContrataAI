04a9accbc501131f89c1065ac4d7356816dfb041
LucasVB2729 <buenolucas259@gmail.com>
2025-03-18 00:13:22 -0300

    Edits HomePage.kt: reestruturação da tela e configuração do colors.xml e do strings.xml

--- app/src/main/java/br/com/fiap/contrataa/screens/HomePage.kt
+++ app/src/main/java/br/com/fiap/contrataa/screens/HomePage.kt
@@ -1,13 +1,9 @@
 package br.com.fiap.contrataa.screens
 
+import br.com.fiap.contrataa.R
 import androidx.compose.material.icons.filled.Home
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
@@ -15,32 +11,16 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.runtime.*
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material.icons.filled.Person
-
-
-// Definição de cores
-val DarkPurple = Color(0xFF57449a)
-val LightPurple = Color(0xFF7B1FA2)
-val BackgroundColor = DarkPurple
-val Button = Color.White
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun HomePage() {
@@ -48,6 +28,7 @@ fun HomePage() {
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             topBar = { TopBar() },
+            bottomBar = { BottomBar() }
         ) { innerPadding ->
             MainContent(Modifier.padding(innerPadding))
         }
@@ -58,8 +39,8 @@ fun HomePage() {
 @Composable
 fun TopBar() {
     CenterAlignedTopAppBar(
-        title = { Text("Boas-Vindas", fontSize = 30.sp, fontWeight = FontWeight.Bold, color = Color.White) },
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = DarkPurple)
+        title = { Text(text = stringResource(id = R.string.boas_vindas), fontSize = 30.sp, fontWeight = FontWeight.Bold, color = colorResource(id = R.color.CorDoTexto)) },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = colorResource(id = R.color.BackgroundRoxo)),
     )
 }
 
@@ -68,127 +49,170 @@ fun MainContent(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(BackgroundColor)
+            .background(colorResource(id = R.color.BackgroundRoxo))
             .padding(16.dp),
         horizontalAlignment = Alignment.Start
     ) {
-        Spacer(modifier = Modifier.height(30.dp))
+        Spacer(modifier = Modifier.height(10.dp))
+        var searchText by remember { mutableStateOf("") }
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            placeholder = { Text("O que você precisa?", fontSize = 20.sp, color = Color.White) },
+            value = searchText,
+            onValueChange = { searchText = it },
+            placeholder = { Text(stringResource(R.string.placeholder1), fontSize = 20.sp, color = Color.White) },
             modifier = Modifier.fillMaxWidth(),
             leadingIcon = {
                 Icon(Icons.Default.Search, contentDescription = "Buscar", tint = Color.White)
-            }
+
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedTextColor = Color.White, // Define a cor do texto quando o campo está focado
+                unfocusedTextColor = Color.White, // Define a
+            )
         )
         Spacer(modifier = Modifier.height(40.dp))
         Card(
             modifier = Modifier.fillMaxWidth(),
-            colors = CardDefaults.cardColors(containerColor = Button)
+            colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.CorDoBotao))
         ) {
             Row(
                 modifier = Modifier.padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Column {
-                    Text("Assistência Técnica", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = DarkPurple)
+                    Text(stringResource(R.string.assistencia_tecnica), fontSize = 20.sp, fontWeight = FontWeight.Bold, color = colorResource(id = R.color.BackgroundRoxo))
                 }
                 Spacer(modifier = Modifier.weight(1f))
-                Icon(Icons.Default.ArrowForward, contentDescription = "Ir", tint = DarkPurple)
+                Icon(Icons.Default.ArrowForward, contentDescription = "Ir", tint = colorResource(id = R.color.BackgroundRoxo))
             }
         }
         Spacer(modifier = Modifier.height(15.dp))
         Card(
             modifier = Modifier.fillMaxWidth(),
-            colors = CardDefaults.cardColors(containerColor = Button)
+            colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.CorDoBotao))
         ) {
             Row(
                 modifier = Modifier.padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Column {
-                    Text("Reformas e Reparos", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = DarkPurple)
+                    Text(stringResource(R.string.reformas_reparos), fontSize = 20.sp, fontWeight = FontWeight.Bold, color = colorResource(id = R.color.BackgroundRoxo))
                 }
                 Spacer(modifier = Modifier.weight(1f))
-                Icon(Icons.Default.ArrowForward, contentDescription = "Ir", tint = DarkPurple)
+                Icon(Icons.Default.ArrowForward, contentDescription = "Ir", tint = colorResource(id = R.color.BackgroundRoxo))
             }
         }
         Spacer(modifier = Modifier.height(15.dp))
         Card(
             modifier = Modifier.fillMaxWidth(),
-            colors = CardDefaults.cardColors(containerColor = Button)
+            colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.CorDoBotao))
         ) {
             Row(
                 modifier = Modifier.padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Column {
-                    Text("Serviços Domésticos", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = DarkPurple)
+                    Text(stringResource(R.string.servicos_domesticos), fontSize = 20.sp, fontWeight = FontWeight.Bold, color = colorResource(id = R.color.BackgroundRoxo))
                 }
                 Spacer(modifier = Modifier.weight(1f))
-                Icon(Icons.Default.ArrowForward, contentDescription = "Ir", tint = DarkPurple)
+                Icon(Icons.Default.ArrowForward, contentDescription = "Ir", tint = colorResource(id = R.color.BackgroundRoxo))
             }
         }
         Spacer(modifier = Modifier.height(15.dp))
         Card(
             modifier = Modifier.fillMaxWidth(),
-            colors = CardDefaults.cardColors(containerColor = Button)
+            colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.CorDoBotao))
         ) {
             Row(
                 modifier = Modifier.padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Column {
-                    Text("Aulas Particulares", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = DarkPurple)
+                    Text(stringResource(R.string.servicos_eletricos), fontSize = 20.sp, fontWeight = FontWeight.Bold, color = colorResource(id = R.color.BackgroundRoxo))
                 }
                 Spacer(modifier = Modifier.weight(1f))
-                Icon(Icons.Default.ArrowForward, contentDescription = "Ir", tint = DarkPurple)
+                Icon(Icons.Default.ArrowForward, contentDescription = "Ir", tint = colorResource(id = R.color.BackgroundRoxo))
             }
         }
         Spacer(modifier = Modifier.height(15.dp))
         Card(
             modifier = Modifier.fillMaxWidth(),
-            colors = CardDefaults.cardColors(containerColor = Button)
+            colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.CorDoBotao))
         ) {
             Row(
                 modifier = Modifier.padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Column {
-                    Text("Eventos", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = DarkPurple)
+                    Text(stringResource(R.string.mudancas_carretos), fontSize = 20.sp, fontWeight = FontWeight.Bold, color = colorResource(id = R.color.BackgroundRoxo))
                 }
                 Spacer(modifier = Modifier.weight(1f))
-                Icon(Icons.Default.ArrowForward, contentDescription = "Ir", tint = DarkPurple)
+                Icon(Icons.Default.ArrowForward, contentDescription = "Ir", tint = colorResource(id = R.color.BackgroundRoxo))
             }
         }
-
         Spacer(modifier = Modifier.height(30.dp))
-        Text("Mais Serviços", fontSize = 25.sp, fontWeight = FontWeight.Bold, color = Color.White)
+        Text(stringResource(R.string.mais_servicos), fontSize = 25.sp, fontWeight = FontWeight.Bold, color = Color.White)
         Spacer(modifier = Modifier.height(10.dp))
-        RepairServices()
+        MoreServices()
     }
 }
 
 @Composable
-fun RepairServices() {
-    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-        listOf("Serviços Elétricos", "Marido de Aluguel", "Mudanças e Carretos").forEach { service ->
-            Card(
-                modifier = Modifier.weight(1f),
-                shape = RoundedCornerShape(8.dp),
-                colors = CardDefaults.cardColors(containerColor = Color.White)
-            ) {
-                Column(modifier = Modifier.padding(8.dp)) {
-                    Box(modifier = Modifier
-                        .height(80.dp)
-                        .fillMaxWidth()
-                        .background(Color.LightGray))
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(service, fontSize = 18.sp, fontWeight = FontWeight.Bold, color = DarkPurple)
-                }
-            }
-        }
+fun MoreServices() {
+    NavigationBar(
+        modifier = Modifier.height (95.dp),
+        containerColor = colorResource(id = R.color.BackgroundRoxo)
+    ) {
+        NavigationBarItem(
+            modifier = Modifier.height (56.dp),
+            icon = { Icon(Icons.Default.AddCircle, contentDescription = "Aulas", tint = Color.White) },
+            label = { Text(stringResource(R.string.aulas), color = Color.White) },
+            selected = false,
+            onClick = { /* TODO */ }
+        )
+        NavigationBarItem(
+            modifier = Modifier.height (56.dp),
+            icon = { Icon(Icons.Default.AddCircle, contentDescription = "Eventos", tint = Color.White) },
+            label = { Text(stringResource(R.string.eventos), color = Color.White) },
+            selected = false,
+            onClick = { /* TODO */ }
+        )
+        NavigationBarItem(
+            modifier = Modifier.height (56.dp),
+            icon = { Icon(Icons.Default.AddCircle, contentDescription = "Marcenaria", tint = Color.White) },
+            label = { Text(stringResource(R.string.marcenaria), color = Color.White) },
+            selected = false,
+            onClick = { /* TODO */ }
+        )
+    }
+}
+
+@Composable
+fun BottomBar() {
+    NavigationBar(
+        modifier = Modifier.height (85.dp),
+        containerColor = colorResource(id = R.color.BackgroundRoxo)
+    ) {
+        NavigationBarItem(
+            modifier = Modifier.height (56.dp),
+            icon = { Icon(Icons.Default.Home, contentDescription = "Inicio", tint = Color.White) },
+            label = { Text(stringResource(R.string.inicio), color = Color.White) },
+            selected = true,
+            onClick = { /* TODO */ }
+        )
+        NavigationBarItem(
+            modifier = Modifier.height (56.dp),
+            icon = { Icon(Icons.Default.ShoppingCart, contentDescription = "Pedidos", tint = Color.White) },
+            label = { Text(stringResource(R.string.pedidos), color = Color.White) },
+            selected = false,
+            onClick = { /* TODO */ }
+        )
+        NavigationBarItem(
+            modifier = Modifier.height (56.dp),
+            icon = { Icon(Icons.Default.Person , contentDescription = "Perfil", tint = Color.White) },
+            label = { Text(stringResource(R.string.perfil), color = Color.White) },
+            selected = false,
+            onClick = { /* TODO */ }
+        )
     }
 }
 
